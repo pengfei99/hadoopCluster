e5fe150a92ea6add4d723e54dc0575362de6b3ba
Pengfei Liu <liu.pengfei@hotmail.fr>
2024-05-28 13:30:52 +0200

    update shell

--- src/tools/xshell.sh
+++ src/tools/xshell.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-# check arg numbers
+# 1. check arg numbers
 if [ $# -lt 1 ]
 then
   echo Not enough Argument
   exit;
 fi
 
-# 2. loop through host list
+# 2. build full command with all args
+for arg in $@
+do
+     command="$command $arg"
+done
+echo $commad
+
+# 3. loop through host list
 for host in spark-w01 spark-w02
 do
   echo =========================== $host ==============================
-
-  # 3. loop through the give file list, sync it one by one
-  for command in $@
-  do
-     # 4. run the commands
-    ssh $host "$command"
-  done
+  # 4. run the commands
+  ssh $host "$command"
 done
